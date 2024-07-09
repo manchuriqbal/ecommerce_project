df6e16a4ebc9f484935ca8c2552939020646a200
manchuriqbal <manchur.fiverr@gmail.com>
2024-07-09 10:34:41 +0600

    using sweet alert

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -82,7 +82,7 @@
                     <tr>
                         <td>{{$categories->category_name}}</td>
                        <td>
-                        <form action="{{route('category.delete', $categories->id)}}" method="post">
+                        <form action="{{route('category.delete', $categories->id)}}" onsubmit="return confirmDelete(event, this)" method="post">
                             @csrf
                             @method('delete')
                             <input class="btn btn-danger" type="submit" value="Delete">
@@ -104,6 +104,33 @@
       </div>
     </div>
     <!-- JavaScript files-->
+    <script>
+        function confirmDelete(event, form) {
+            event.preventDefault();
+
+            swal({
+                title: "Are you sure?",
+                text: "Are you sure that you want to Delete this Category?",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+                })
+                .then(willDelete => {
+                if (willDelete) {
+                    form.submit();
+                    swal("Deleted!", "Your imaginary file has been deleted!", "warning");
+                }
+                else{
+                    swal("Safe!", "Your imaginary file Are Deleted!", "success");
+
+                }
+                });
+
+        }
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <script src=" {{asset('adminsite/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
     <script src=" {{asset('adminsite/vendor/jquery/jquery.min.js')}}"></script>
     <script src=" {{asset('adminsite/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
